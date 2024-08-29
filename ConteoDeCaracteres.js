6c942b29c634c2d7b6108802f472c6684e3d8dac
KaryMC18 <kayamacedoca@ittepic.edu.mx>
2024-08-28 23:06:05 -0700

    first

---
+++ ConteoDeCaracteres.js
@@ -0,0 +1,9 @@
+function contador(cadena) {
+    const numCaracteres = cadena.length;
+
+    console.log(`La cadena "${cadena}" tiene ${numCaracteres} caracteres.`);
+}
+
+  const cadena = "Hola, mundo!";
+  contador(cadena);
+  
